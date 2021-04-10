7a859f0003f2c1f63b58201968de35cc4be52b06
Peter Nguyen <p3t3rn9uy3n@gmail.com>
2021-04-09 23:25:30 -0500

    added array of questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the name of the project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What was your motivation?",
+        name: "motivation"
+    },
+    {
+        type: "input",
+        message: "Why did you build this project?",
+        name: "why"
+    },
+    {
+        type: "input",
+        message: "What problem does it solve?",
+        name: "problem"
+    },
+    {
+        type: "input",
+        message: "What did you learn?",
+        name: "learn"
+    },
+    {
+        type: "input",
+        message: "What makes your project stand out?",
+        name: "standout"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
